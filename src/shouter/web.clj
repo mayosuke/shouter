da99f3680a362b7e08d2864e8d48f21d9fb48551
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2015-05-05 13:50:27 +0900

    Add hiccup support

--- src/shouter/web.clj
+++ src/shouter/web.clj
@@ -1,9 +1,17 @@
 (ns shouter.web
   (:require [compojure.core :refer [defroutes GET]]
-            [ring.adapter.jetty :as ring]))
+            [ring.adapter.jetty :as ring]
+            [hiccup.page :as page]))
+
+(defn index []
+  (page/html5
+    [:head
+      [:title "Hello World"]]
+    [:body
+      [:div {:id "content"} "Hello World"]]))
 
 (defroutes routes
-  (GET "/" [] "<h2>Hello World</h2>"))
+  (GET "/" [] (index)))
 
 (defn -main []
   (ring/run-jetty #'routes {:port 8080 :join? false}))
